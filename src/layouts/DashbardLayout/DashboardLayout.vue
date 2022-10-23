777970f6952ecab81c69c267895efca959b8dc5c
Sultan Mustafin <sultan.fb29@gmail.com>
2022-10-24 01:05:12 +0300

    feat: create basic application structure and layout

---
+++ src/layouts/DashbardLayout/DashboardLayout.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import DashboardHeader from "@/layouts/DashbardLayout/components/DashboardHeader.vue";
+</script>
+
+<template>
+  <dashboard-header />
+  <RouterView />
+</template>
---
+++ src/layouts/DashbardLayout/components/DashboardHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<style scoped>
+.header {
+  height: 70px;
+  width: 100vw;
+  background: var(--primary-color);
+}
+</style>
