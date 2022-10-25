4b0868ffb61d6d9494a8b48e24bf453c2859e92c
Sultan Mustafin <sultan.fb29@gmail.com>
2022-10-25 16:51:36 +0300

    feat: create ideas feed and idea compoenents

--- src/modules/ideas-feed/IdeasFeedView.vue
+++ src/modules/ideas-feed/IdeasFeedView.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import IdeaCard from "@/modules/ideas-feed/components/IdeaCard.vue";
+import { ideas } from "@/modules/ideas-feed/composables/fetchIdeasFeed";
+</script>
+
 <template>
-  <h1>Лента идеи</h1>
+  <div class="feed">
+    <IdeaCard
+      v-for="idea of ideas"
+      :title="idea.title"
+      :description="idea.description"
+      :score="idea.score"
+      :image-url="idea.imageUrl"
+    />
+  </div>
 </template>
