62748d8da2c4fbd3471c9393a66eea6fa4643ce2
Mukhaman <mukhamanzhol@gmail.com>
2022-10-25 17:37:04 +0300

    feat: add color change of hovered button

--- src/layouts/DashbardLayout/components/DashboardHeader/DashboardHeader.vue
+++ src/layouts/DashbardLayout/components/DashboardHeader/DashboardHeader.vue
@@ -1,16 +1,16 @@
 <template>
   <header class="header">
-    <div class="header__logo">
+    <div class="header__left">
       <img src="@/assets/icons/logo.svg">
       <p class="header__name">Ferrum</p>
     </div>
-    <div class="header__profile">
-      <button class="header__button">
-        <img src="@/assets/icons/archive.svg" id="but">
+    <div class="header__right">
+      <button class="header__archive">
+        <img src="@/assets/icons/archive.svg" class="header__archive_active">
         <img src="@/assets/icons/archive-hovered.svg">
       </button>
-      <button class="header__button">
-        <img src="@/assets/icons/profile-picture.svg" id="but">
+      <button class="header__profile">
+        <img src="@/assets/icons/profile-picture.svg" class="header__profile_active">
         <img src="@/assets/icons/profile-picture-hovered.svg">
       </button>
     </div>
@@ -28,37 +28,58 @@
   width: 100vw;
   background: var(--primary-color);
 }
-.header__logo{
+.header__left{
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin: 0 0 0 40px;
+  margin-left: 40px;
   justify-content: space-between;
 }
 .header__name {
   font-size: 22px;
   font-weight: 700;
   color: #ffffff;
-  margin: 0 0 0 15px;
+  margin-left: 15px;
 }
-.header__profile {
+.header__right {
   display: flex;
   flex-direction: row;
-  margin: 0 20px 0 0;
+  margin-right: 30px;
 }
-.header__button {
-  width: 70px;
-  height: 50px;
-  background-color: var(--primary-color);
+.header__archive, .header__profile{
+  position: relative;
+  margin-left: 15px;
+  width: 55px;
+  height: 55px;
+  background: var(--primary-color);
   border: none;
+  border-radius: 50%;
+  transition-duration: 0.3s;
 }
-
-#but {
+.header__archive::after {
+  position: absolute;
+  right: 8px;
+  top: 10px;
+  content: "";
+  display: inline-block;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  background:var(--secondary-color);
+}
+.header__archive:hover, .header__profile:hover{
+  background: var(--dark-grey-color);
+  cursor: pointer;
+}
+.header__archive_active, .header__profile_active  {
   position: absolute;
   opacity: 1;
   transition-duration: 0.3s;
 }
-.header__button:hover #but {
+.header__profile:hover .header__profile_active {
+  opacity: 0;
+}
+.header__archive:hover .header__archive_active {
   opacity: 0;
 }
 
