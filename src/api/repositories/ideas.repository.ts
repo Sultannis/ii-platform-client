3672e69c966c394431469a8998ade144a6300e7e
Sultan Mustafin <sultan.fb29@gmail.com>
2022-11-16 15:30:21 +0300

    feat: create fetch ideas repository method

--- src/api/repositories/ideas.repository.ts
+++ src/api/repositories/ideas.repository.ts
@@ -1,10 +1,11 @@
-import type { UserDao } from "../dao/User.dao";
-import type { LoginUserDto } from "../converters/login-user/LoginUser.dto";
-import { mapRegisterUserDtoToDao } from "../converters/register-user/registerUserMapper";
-import { mapUserDaoToEntity } from "../mappers/userMappers";
 import request from "../request";
+import type { UserDao } from "../dao/User.dao";
+import type { IdeaDao } from "../dao/Idea.dao";
+import type { LoginUserDto } from "../converters/login-user/LoginUser.dto";
 import type { QueryParamsDto } from "../converters/query-params/QueryParams.dto";
 import { mapQueryParamsForRequest } from "../converters/query-params/queryParamsMapper";
+import { mapUserDaoToEntity } from "../mappers/userMappers";
+import { mapIdeaDaoToEntity } from "../mappers/ideaMapper";
 
 export const fetchIdeas = (queryParams: QueryParamsDto) =>
   request
@@ -12,22 +13,7 @@ export const fetchIdeas = (queryParams: QueryParamsDto) =>
       params: mapQueryParamsForRequest(queryParams),
     })
     .then((response) => {
-      const authDao = response.data.auth;
-      const userDao = response.data.user as UserDao;
+      const ideaDaos = response.data.ideas as IdeaDao[];
 
-      const authToken = authDao.token;
-      const user = mapUserDaoToEntity(userDao);
-
-      return [user, authToken];
+      return ideaDaos.map(mapIdeaDaoToEntity);
     });
-
-export const loginUser = (payload: LoginUserDto) =>
-  request.post("/users/login", payload).then((response) => {
-    const authDao = response.data.auth;
-    const userDao = response.data.user as UserDao;
-
-    const authToken = authDao.token;
-    const user = mapUserDaoToEntity(userDao);
-
-    return [user, authToken];
-  });
