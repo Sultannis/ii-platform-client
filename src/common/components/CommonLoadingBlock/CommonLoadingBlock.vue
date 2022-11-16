4fdc26e490c6d722f649b1dcc9cc00ec6b83935a
Sultan Mustafin <sultan.fb29@gmail.com>
2022-11-16 19:55:25 +0300

    feat: create loading indicator component

---
+++ src/common/components/CommonLoadingBlock/CommonLoadingBlock.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="loading">
+    <div class="loading__indicator"></div>
+  </div>
+</template>
+
+<style scoped>
+.loading {
+  height: 80px;
+  width: 100%;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #fff;
+}
+
+.loading__indicator {
+  width: 48px;
+  height: 48px;
+  display: inline-block;
+  position: relative;
+}
+
+.loading__indicator::after,
+.loading__indicator::before {
+  content: "";
+  box-sizing: border-box;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  background: var(--primary-color);
+  position: absolute;
+  left: 0;
+  top: 0;
+  animation: animloader 2s linear infinite;
+}
+
+.loading__indicator::after {
+  animation-delay: 1s;
+}
+
+@keyframes animloader {
+  0% {
+    transform: scale(0);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+}
+</style>
--- src/modules/feed/ideas-feed/IdeasFeedView.vue
+++ src/modules/feed/ideas-feed/IdeasFeedView.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
+import { useElementVisibility } from "@vueuse/core";
 import IdeaCard from "@/modules/feed/ideas-feed/components/IdeaCard/IdeaCard.vue";
+import CommonLoadingBlock from "@/common/components/CommonLoadingBlock/CommonLoadingBlock.vue";
 import { ideas } from "@/modules/feed/ideas-feed/composables/fetchIdeasFeed";
+import { ref, watch } from "vue";
+
+const feedLoadingTrigger = ref(null);
+const feedLoadingTriggerVisible = useElementVisibility(feedLoadingTrigger);
+
+watch(feedLoadingTriggerVisible, () => {
+  console.log(feedLoadingTriggerVisible.value);
+});
 </script>
 
 <template>
@@ -13,6 +23,10 @@ import { ideas } from "@/modules/feed/ideas-feed/composables/fetchIdeasFeed";
       :score="idea.score"
       :image-url="idea.mainImageUrl"
     />
+    <div style="width: 100%; height: 200vh"></div>
+
+    <CommonLoadingBlock />
+    <div ref="feedLoadingTrigger" class="feed__loading-trigger" />
   </div>
 </template>
 
